06bdefc159ced6bff9663b784612bdc3999e44cd
Chas <chas.narne@gmail.com>
2018-07-01 08:52:08 -0400

    Removed obsolete comments

--- options.js
+++ options.js
@@ -1,6 +1,4 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Removed obsolete comments
 debugger;
 function createForm() {
   //need a get for env options
